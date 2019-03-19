bcbca6edfbcbdf83cf0d84849ceea19ca7c9c901
articlol <neifylol@gmail.com>
2019-03-19 10:29:17 +0300

    2

--- src/SortBinary.java
+++ src/SortBinary.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
 public class SortBinary {
+    public static void main(String[] args){
+        System.out.println(poiskBinary(new int[]{1,3,4,6,19,21,30,44}, 4));
+    }
+    public static int poiskBinary(int[]x, int iskomoe){
+        int low = 0;
+        int high = x.length - 1;
 
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            if (iskomoe < x[mid]){
+                high = mid - 1;
+            } else if (iskomoe > x[mid]){
+                low = mid + 1;
+            } else{
+                return mid;
+            }
+        }
+        return 0;
+    }
 }
+
--- src/SortSelect.java
+++ src/SortSelect.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class SortSelect {
+    public static void main(String[] args){
+        int [] a = {40,30,2,14,5,16,2,46,17,87,29,43,11,1};
+        for (int i = 0; i < a.length; i++){
+            int min = a[i];
+            int imin = i;
+            for (int j = i + 1; j < a.length; j++){
+                if (a[j] < min){
+                    min = a[j];
+                    imin = j;
+                }
+            }
+            if (i!=imin){
+                int t = a[i];
+                a[i] = a[imin];
+                a[imin]= t;
+            }
+            System.out.println(a[i]);
+        }
+    }
 }
