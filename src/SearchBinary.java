e6a40ded9d49fb6c96a581f57f4013e6f2972e1c
articlol <neifylol@gmail.com>
2019-03-19 11:37:29 +0300

    clean code

--- src/SearchBinary.java
+++ src/SearchBinary.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class SortBinary {
-    public static void main(String[] args){
-        System.out.println(poiskBinary(new int[]{1,3,4,6,19,21,30,44}, 4));
+    public static void main(String[] args) {
+        System.out.println(poiskBinary(new int[]{1, 3, 4, 6, 19, 21, 30, 44}, 4));
     }
-    public static int poiskBinary(int[]x, int iskomoe){
+
+    public static int poiskBinary(int[] x, int findX) {
         int low = 0;
         int high = x.length - 1;
 
-        while (low <= high){
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (iskomoe < x[mid]){
+            if (findX < x[mid]) {
                 high = mid - 1;
-            } else if (iskomoe > x[mid]){
+            } else if (findX > x[mid]) {
                 low = mid + 1;
-            } else{
+            } else {
                 return mid;
             }
         }
