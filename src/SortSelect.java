bf277480f84d0739b18b9c8eb3a8315e39537d5e
articlol <neifylol@gmail.com>
2019-03-19 11:41:18 +0300

    cleanCode

--- src/SortSelect.java
+++ src/SortSelect.java
@@ -1,19 +1,19 @@
 public class SortSelect {
-    public static void main(String[] args){
-        int [] a = {40,30,2,14,5,16,2,46,17,87,29,43,11,1};
-        for (int i = 0; i < a.length; i++){
+    public static void main(String[] args) {
+        int[] a = {40, 30, 2, 14, 5, 16, 2, 46, 17, 87, 29, 43, 11, 1};
+        for (int i = 0; i < a.length; i++) {
             int min = a[i];
-            int imin = i;
-            for (int j = i + 1; j < a.length; j++){
-                if (a[j] < min){
+            int i_min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < min) {
                     min = a[j];
-                    imin = j;
+                    i_min = j;
                 }
             }
-            if (i!=imin){
+            if (i != i_min) {
                 int t = a[i];
-                a[i] = a[imin];
-                a[imin]= t;
+                a[i] = a[i_min];
+                a[i_min] = t;
             }
             System.out.println(a[i]);
         }
