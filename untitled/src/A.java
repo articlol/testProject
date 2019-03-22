8df25fb62ee4b97a5ae2c17c3351ba973d648879
articlol <neifylol@gmail.com>
2019-03-22 14:36:14 +0300

    fafa

--- untitled/src/A.java
+++ untitled/src/A.java
@@ -2,19 +2,29 @@ import java.util.Date;
 
 
 final class A {
-    private int i;
-    private Date d;
+    private final int i;
+    private final Date d;
 
     public A(int i, Date d) {
         this.i = i;
-        this.d = d;
+        this.d = (d == null) ? null : new Date(d.getTime());
     }
+
     public int getI() {
 
         return this.i;
     }
+
     public Date getD() {
 
-        return new Date(this.d.getTime());
+        return (d == null) ? null : new Date(d.getTime());
     }
+
+    public static void main(String[] args) {
+        Date d = new Date();
+        A a = new A(3,d);
+        d.setTime(0);
+        System.out.println(a.getD());
+    }
+
 }
