78f80ec47ec9b3e264b5b1be33e5da0188207ca3
articlol <neifylol@gmail.com>
2019-03-28 13:01:51 +0300

    Использование steam api

---
+++ untitled/src/Car.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Car {
+    private String number;
+    private int year;
+
+    public Car(String number, int year) {
+        this.number = number;
+        this.year = year;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public static void main(String[] args) {
+        List<Car> cars = Arrays.asList(
+                new Car("AA1111BX", 2007),
+                new Car("AK5555IT", 2010),
+                new Car(null, 2012),
+                new Car("", 2015),
+                new Car("AI3838PP", 2017));
+
+        /*
+        for (Car car : cars) {          ----   стандартно
+        if (car.getYear() > 2010) {
+            String number = car.getNumber();
+            if (number != null && !number.isEmpty())
+                System.out.println(number);
+        }
+        */
+        cars.stream()                     //    -------  использование steam api
+                .filter(y -> y.getYear() > 2010)
+                .map(Car::getNumber).filter(x -> x != null && !x.isEmpty())
+                .forEach(System.out::println);
+
+
+        }
+    }
+
